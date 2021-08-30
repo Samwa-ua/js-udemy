266ea05c586d22c03599cb6fc3be3edb9c2ade05
Samwa-ua <askokovskiy@gmail.com>
2021-08-30 21:32:17 +0300

    modified js.script

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
-// let age = prompt("How old are you?");
-// age >= 18 ? alert("I like to drink 🍺") : alert("i like to drink 💧");
 
-// const age = 36;
-// alert(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
 
 //Challenge #4
 
-const bill = parseInt(prompt("Enter the sun you need to pay"));
-const tip = bill <= 50 ? bill * 0.15 : bill * 0.2;
-alert(`The bill was ${bill}, the tip was ${Math.round(tip)}, and the total value is ${Math.round(bill + tip)}`);
+// const bill = parseInt(prompt("Enter the sum you need to pay"));
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// alert(`The bill was ${bill}, the tip was ${Math.round(tip)}, and the total value is ${Math.round(bill + tip)}`);
+
+// function logger() {
+//     console.log("My name is Andy");
+// }
+//calling, running, invokking function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges. `;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+calcAge1(1984);
